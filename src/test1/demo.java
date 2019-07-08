8ed21346c4eb7b84d34dbc49e4f15d2f22d7195c
hefy <282971962@qq.com>
2019-07-08 23:40:00 +0800

    git管理测试

---
+++ src/test1/demo.java
@@ -0,0 +1,12 @@
+package test1;
+/**
+* @author hefy:
+* @date 创建时间：2019年7月8日 下午11:38:56
+* @version 1.0
+* 类说明
+*/
+public class demo {
+	public static void main(String[] args) {
+		System.out.println("测试");
+	}
+}
