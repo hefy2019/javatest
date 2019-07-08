702ac18f25938344f05f32815df20d3dd538f18d
hefy <282971962@qq.com>
2019-07-08 23:42:28 +0800

    管理演示

---
+++ src/test1/Demo2.java
@@ -0,0 +1,14 @@
+package test1;
+/**
+* @author hefy:
+* @date 创建时间：2019年7月8日 下午11:41:36
+* @version 1.0
+* 类说明
+*/
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git管理测试");
+	}
+}
